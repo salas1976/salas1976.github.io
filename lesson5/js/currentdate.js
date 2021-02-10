4c9c5b516ab87d7bd27cbd41ee343043fbdb4133
Your Name <salas76@byui.edu>
2021-02-09 19:05:33 -0700

    chnage

--- lesson5/js/currentdate.js
+++ lesson5/js/currentdate.js
@@ -1,2 +1,39 @@
-var updatedDate = "Last Updated: " + document.lastModified;
-document.getElementById("updatetime").textContent = updatedDate;
+const d = new Date();
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById("update").textContent = fulldate;
+
+let dayOfWeek = new Date();
+const banner = document.getElementById("alert");
+if (dayOfWeek.getDay() == 5) {
+    banner.style.display = "content";
+} else {
+    banner.style.display = "none";
+}
