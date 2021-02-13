98673516ec91a1e5a9ce3cfd4caec9f8e68ad607
Your Name <salas76@byui.edu>
2021-02-13 13:35:28 -0700

    change

--- lesson6/js/currentdate.js
+++ lesson6/js/currentdate.js
@@ -11,7 +11,3 @@ console.log(today);
 const dayNumber = today.getDay();
 console.log(dayNumber);
 
-const element = document.getElementById("message");
-if (dayNumber == 5) {
-    element.classList.add("showme");
-}
