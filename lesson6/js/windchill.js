1e33259c62fb4840a2038bc7c0df2cc0efa0bfd3
Your Name <salas76@byui.edu>
2021-02-12 19:54:10 -0700

    chnage

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,14 +1,18 @@
-window.onload = function windChill() {
-  let temp = parseFloat(document.getElementById("currTemp").textContent);
-  let speed = parseFloat(document.getElementById("windSpeed").textContent);
-  let output = "N/A";
-  if (temp <= 50 && speed >= 3) {
-    let f =
-      35.74 +
-      0.6215 * temp -
-      35.75 * Math.pow(speed, 0.16) +
-      0.4275 * (temp * Math.pow(speed, 0.16));
-    output = Math.round(f);
-  }
-  document.getElementById("chill").innerHTML = output + " &#8457;";
-};
+const tempNumber = parseFloat(document.getElementById('temp').textContent);
+// console.log(tempNumber)
+
+const speedNumber = parseFloat(document.getElementById('speed').textContent);
+// console.log(speedNumber)
+
+let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16))
++ (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+// console.log(windchill);
+windchill = Math.round(windchill);
+// console.log(windchill);
+
+if (tempNumber <= 50 && speedNumber > 3) {
+    document.getElementById('chill').textContent = "Wind Chill is "+windchill+"\xB0F";
+}
+else {
+    document.getElementById('chill').textContent = "No Wind Chill Today"
+}
