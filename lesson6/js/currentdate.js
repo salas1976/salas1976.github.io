7af70db048cadd9628067fd545945cfd2294041a
Your Name <salas76@byui.edu>
2021-02-13 13:17:39 -0700

    change

--- lesson6/js/currentdate.js
+++ lesson6/js/currentdate.js
@@ -1,2 +1,17 @@
-var updatedDate = "Last Updated: " + document.lastModified;
-document.getElementById("updatetime").textContent = updatedDate;
+const save_date = new Date();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var todayDate = days[save_date.getDay()] + ", " + save_date.getDate() + " " + months[save_date.getMonth()] + " " + save_date.getFullYear()
+document.getElementById("updatetime").textContent = todayDate;
+
+// displays the message on saturday only
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay();
+console.log(dayNumber);
+
+const element = document.getElementById("message");
+if (dayNumber == 5) {
+    element.classList.add("showme");
+}
