534001b566ca303277c3eb38f5e5c681ffbe90ae
Your Name <salas76@byui.edu>
2021-03-01 20:00:59 -0700

    change

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,14 +5,40 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    for (let i = 0; i < prophets.length; i++ ) {}
-    });
-  const prophets = jsonObject['prophets'];
-  let card = document.createElement('section');
-let h2 = document.createElement('h2');
+    const prophets = jsonObject['prophets'];
+    
+    for (let i = 0; i < prophets.length; i++ ) {
+   
+    let card = document.createElement('section');
+    let titleDiv = document.createElement('div');
+    titleDiv.classList.add('card-title');
+    let bodyDiv = document.createElement('div');
+    bodyDiv.classList.add('card-body');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+    // h2 - name
+    let fullName = document.createElement('h2');
+    fullName.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+    titleDiv.appendChild(fullName);
 
-card.appendChild(h2);
+    // p - birthdate
+    let dateOfBirth = document.createElement('p');
+    dateOfBirth.textContent = 'Date of Birth:\r\n ' + prophets[i].birthdate;
+    bodyDiv.appendChild(dateOfBirth);
 
-document.querySelector('div.cards').appendChild(card);
+    // p - birthplace
+    let placeOfBirth = document.createElement('p');
+    placeOfBirth.textContent = 'Place of Birth:\r\n' + prophets[i].birthplace;
+    bodyDiv.appendChild(placeOfBirth);
+
+    // img - picture
+    let image = document.createElement('img');
+    image.setAttribute('src', prophets[i].imageurl);
+    image.setAttribute('loading', 'lazy');
+    image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + (i + 1));
+    card.appendChild(titleDiv);
+    card.appendChild(image);
+    card.appendChild(bodyDiv);
+
+    document.querySelector('div.cards').appendChild(card);
+   }
+});
