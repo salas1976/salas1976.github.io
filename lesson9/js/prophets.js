cdb48aa611a811f9b713edba7f83f50b04dfe26a
Your Name <salas76@byui.edu>
2021-03-01 19:26:35 -0700

    chAGE

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,4 +5,14 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-  });
+    for (let i = 0; i < prophets.length; i++ ) {}
+    });
+  const prophets = jsonObject['prophets'];
+  let card = document.createElement('section');
+let h2 = document.createElement('h2');
+
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+card.appendChild(h2);
+
+document.querySelector('div.cards').appendChild(card);
