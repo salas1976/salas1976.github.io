56a1b112ba9433b7193aa4c9dc01e1fbb8ac1778
Your Name <salas76@byui.edu>
2021-03-10 19:11:55 -0700

    chnage

--- lesson9/js/townndata.js
+++ lesson9/js/townndata.js
@@ -1,4 +1,4 @@
-const requestURL = '../lesson09/json/towndata.json';
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 fetch(requestURL)
 .then(function (response) {
    return response.json();
