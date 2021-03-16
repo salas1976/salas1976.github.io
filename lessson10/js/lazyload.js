99ca24c3916870c6d863f1b116fc8e78b5aa09ff
Your Name <salas76@byui.edu>
2021-03-15 19:36:18 -0600

    change

--- lessson10/js/lazyload.js
+++ lessson10/js/lazyload.js
@@ -1,22 +1,32 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+
+
+const imgOptions = {
+    threshold: 0.5
+};
+
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
-        image.removeAttribute('data-src');
+        image.removeAttribute("data-src");
     };
 };
 
+
 if ('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
+    const imgObserver = new IntersectionObserver ((items, imgObserver) => {
         items.forEach((item) => {
-            if (item.isIntersecting) {
+            if (item.isIntersecting){
                 loadImages(item.target);
-                observer.unobserve(item.target);
+                imgObserver.unobserve(item.target);
             }
         });
-    });
+    },imgOptions);
+
+
     imagesToLoad.forEach((img) => {
-        observer.observe(img);
+        imgObserver.observe(img);
     });
 } else {
     imagesToLoad.forEach((img) => {
