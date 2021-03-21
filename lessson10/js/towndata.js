b0f2305aaa8eae0c7cd830e99fc5240ab486d825
Your Name <salas76@byui.edu>
2021-03-20 19:14:47 -0600

    change

--- lessson10/js/towndata.js
+++ lessson10/js/towndata.js
@@ -6,7 +6,7 @@ fetch(requestURL)
 .then(function (jsonObject) {
   const towns = jsonObject['towns'];
   for (let i = 0; i < towns.length; i++) {
-      if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+      if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven") {
         let card = document.createElement('section');
         let name = document.createElement('h2');
         let motto = document.createElement('h4');
